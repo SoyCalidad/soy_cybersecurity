434a98fce713dec92388ccd9f5698533d990967f
Soy Calidad <soycalidad.20@gmail.com>
2025-02-13 14:00:28 -0500

    [ADD] soy_cybersecurity_multi_company

---
+++ soy_cybersecurity_multi_company/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Multi-company Support',
+    'version': '1.0',
+    'description': 'Multi-company Support',
+    'summary': 'Multi-company Support',
+    'author': 'Soy Calidad',
+    'website': 'https://www.soycalidad.com',
+    'license': 'Other proprietary',
+    'category': 'soycalidad',
+    'depends': [
+        'soy_cybersecurity_cybersecurity',
+    ],
+    'data': [
+        'security/security.xml',
+        'views/cybersecurity_views.xml',
+    ],
+    'demo': [
+    ],
+    'auto_install': False,
+    'application': False,
+}
---
+++ soy_cybersecurity_multi_company/models/cybersecurity.py
@@ -0,0 +1,127 @@
+from odoo import api, fields, models
+
+
+class Categ(models.Model):
+    _inherit = 'cyber_2matrix.categ'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class Matrix(models.Model):
+    _inherit = 'cyber_2matrix.matrix'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class Block(models.Model):
+    _inherit = 'cyber_2matrix.block'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class Checklist(models.Model):
+    _inherit = 'cyber_2matrix.checklist'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class ChecklistControl(models.Model):
+    _inherit = 'cyber_2matrix.checklist.control'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class ChecklistLine(models.Model):
+    _inherit = 'cyber_2matrix.checklist.line'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class Line(models.Model):
+    _inherit = 'cyber_2matrix.block.line'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class Categ(models.Model):
+    _inherit = 'cyber_matrix.categ'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class Matrix(models.Model):
+    _inherit = 'cyber_matrix.matrix'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class Block(models.Model):
+    _inherit = 'cyber_matrix.block'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class LineAgent(models.Model):
+    _inherit = 'cyber_matrix.block.line.agent'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class MatrixBlockLineSystem(models.Model):
+    _inherit = 'cyber_matrix.block.line.system'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class MatrixBlockLineSystem(models.Model):
+    _inherit = 'cyber_matrix.block.line.resource'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class MatrixBlockLineSystem(models.Model):
+    _inherit = 'cyber_matrix.block.line.location'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class MatrixBlockLineSystem(models.Model):
+    _inherit = 'cyber_matrix.block.line.language'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class Line(models.Model):
+    _inherit = 'cyber_matrix.block.line'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class Result(models.Model):
+    _inherit = 'cyber_evaluation.result'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class incidentCateg(models.Model):
+    _inherit = 'incident.categ'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class incidentVia(models.Model):
+    _inherit = 'incident.via'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class incidentQuickAction(models.Model):
+    _inherit = 'incident.quick.action'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
+
+
+class incident(models.Model):
+    _inherit = 'incident.incident'
+
+    company_id = fields.Many2one('res.company', string='Compañia', default=lambda self: self.env.company)
